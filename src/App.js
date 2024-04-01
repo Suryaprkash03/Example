8c21679b034c47c8e6b98267d6975cd204db6f51
Suryaprkash03 <suryatheking5784@gmail.com>
2024-04-01 15:52:54 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    email: '',
+    mobileno: '',
+    address: ''
+  });
+  const [showNewButton, setShowNewButton] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowNewButton(true);
+    }, 1200);
+    return
+  }, []);
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="App">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstname" className="form-label">
+            FirstName
+          </label>
+          <input
+            className="form-control"
+            name="firstname"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            className="form-control"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobileno" className="form-label">
+            Mobile No
+          </label>
+          <input
+            className="form-control"
+            name="mobileno"
+            value={formData.mobileno}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input
+            className="form-control"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </div>
+        {showNewButton && (
+          <div className="form-group">
+            <button className="btn new-button">
+              New Button
+            </button>
+          </div>
+        )}
+      </form>
     </div>
+    </>
   );
+
 }
 
+
 export default App;
+
